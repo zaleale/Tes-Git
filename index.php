860a574e959cf971f148b9459189bb7b364dc3d6
zaleale <muhamadrizalnaufalghazi@gmail.com>
2020-12-08 14:11:11 +0700

    test first commit

---
+++ index.php
@@ -0,0 +1,196 @@
+<section class="content-header">
+    <h1>
+        Dashboard
+        <small>Control Panel</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">Dashboard BARU</li>
+    </ol>
+</section>
+<BR></BR>
+
+  <!-- Info boxes -->
+  <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-archive"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Product Knowledge</span>
+                    <span class="info-box-number">29</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Omni</span>
+                    <!-- <span class="info-box-number"><?= $this->fungsi->count_user() ?></span> -->
+                    <span class="info-box-number">28</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="fa fa-desktop"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Helpdesk</span>
+                    <span class="info-box-number">11</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="fa fa-phone"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Halopos</span>
+                    <span class="info-box-number">18</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+
+
+<!-- START CALENDAR -->
+<!-- Main content -->
+<section class="content">
+<html>
+<head>
+<link rel="shortcut icon" href="<?= base_url() ?>assets/dist/css/favicon.ico">
+    <title>Pos Indonesia | Omnichannel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" /> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <script>
+    $(document).ready(function(){
+        var calendar = $('#calendar').fullCalendar({
+            editable:true,
+            header:{
+                left:'prev,next today',
+                center:'title',
+                right:'month,agendaWeek,agendaDay'
+            },
+            events:"<?php echo base_url(); ?>fullcalendar/load",
+            selectable:true,
+            selectHelper:true,
+            select:function(start, end, allDay)
+            {
+                var title = prompt("Enter Event Title");
+                if(title)
+                {
+                    var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                    $.ajax({
+                        url:"<?php echo base_url(); ?>fullcalendar/insert",
+                        type:"POST",
+                        data:{title:title, start:start, end:end},
+                        success:function()
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert("Added Successfully");
+                        }
+                    })
+                }
+            },
+            editable:true,
+            eventResize:function(event)
+            {
+                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+
+                var title = event.title;
+
+                var id = event.id;
+
+                $.ajax({
+                    url:"<?php echo base_url(); ?>fullcalendar/update",
+                    type:"POST",
+                    data:{title:title, start:start, end:end, id:id},
+                    success:function()
+                    {
+                        calendar.fullCalendar('refetchEvents');
+                        alert("Event Update");
+                    }
+                })
+            },
+            eventDrop:function(event)
+            {
+                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+                //alert(start);
+                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+                //alert(end);
+                var title = event.title;
+                var id = event.id;
+                $.ajax({
+                    url:"<?php echo base_url(); ?>fullcalendar/update",
+                    type:"POST",
+                    data:{title:title, start:start, end:end, id:id},
+                    success:function()
+                    {
+                        calendar.fullCalendar('refetchEvents');
+                        alert("Event Updated");
+                    }
+                })
+            },
+            eventClick:function(event)
+            {
+                if(confirm("Are you sure you want to remove it?"))
+                {
+                    var id = event.id;
+                    $.ajax({
+                        url:"<?php echo base_url(); ?>fullcalendar/delete",
+                        type:"POST",
+                        data:{id:id},
+                        success:function()
+                        {
+                            calendar.fullCalendar('refetchEvents');
+                            alert('Event Removed');
+                        }
+                    })
+                }
+            }
+        });
+    });
+             
+    </script>
+</head>
+    
+    <body>
+        <br />
+        <br />
+        <div class="container">
+            <div id="calendar"></div>
+        </div>
+    </body>
+</html>
+<!-- END CALENDAR -->
+
+  
+
+
